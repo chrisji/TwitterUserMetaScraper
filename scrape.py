a43a0ee3f3e38dc51371f8a883745eb45bff7351
chrisji <chrisji@hotmail.co.uk>
2015-04-07 18:24:33 +0100

    Refactoring for modularity

--- scrape.py
+++ scrape.py
@@ -1,46 +1,81 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
 import time
-import datetime
+import json
 import re
+import os
 
-# Location of scrape output.
-output_filename = 'scrape_' + time.strftime('%H-%M-%S_%d%b%Y') + '.txt'
 
-# Number of seconds to wait before sending next user request.
-time_between_requests = 0
+class TwitterMetaScraper():
+    def __init__(self, time_between_reqs=0, output_dir='output', input_dir='input', input_filename='id_list.txt'):
+        self.time_between_reqs = time_between_reqs  # Number of seconds between user requests.
 
-# Get a list of user ids to process.
-user_ids = []
-with open('input/user_id_list.txt', 'r') as f:
-    for line in f.readlines():
-        user_id = line.split(',')[0].strip()
-        if user_id != '':
-            user_ids.append(user_id)
+        # Paths for I/O
+        output_filename = 'scrape_' + time.strftime('%H-%M-%S_%d%b%Y') + '.txt'
+        self.output_path = os.path.join(os.path.dirname(__file__), output_dir, output_filename)
+        self.input_path = os.path.join(os.path.dirname(__file__), input_dir, input_filename)
 
-# Keep track of number processed
-number_processed = 0
+        self.user_ids = self.load_users()  # Load in user ids to process.
 
-# Store number of users to process
-num_of_users = len(user_ids)
+    def run(self):
+        number_processed = 0  # Keep track of number processed
+        num_of_users = len(self.user_ids)  # Store number of users to process
 
-print 'Starting scraper:', datetime.datetime.now()
-for user_id in user_ids:
-    r = requests.get('https://twitter.com/intent/user?user_id=' + user_id)
-    with open('output/' + output_filename, 'a') as f:
-        if r.status_code == 200:
-            data = BeautifulSoup(r.content).find('title').string
-            screen_match = re.search('\((@.*)\)', data)  # Match for screen name
-            name_match = re.search('(.*)\(@', data)  # Match for full name
-            if screen_match and name_match:
-                screen_name = screen_match.group(1).encode('utf-8')
-                full_name = name_match.group(1).strip().encode('utf-8')
-                f.write(user_id + ', ' + screen_name + ', ' + full_name + '\n')
-            else:
-                f.write(user_id + ', N/A, N/A\n')
+        print 'Starting scraper:', datetime.now()
+        for user_id in self.user_ids:
+            self.scrape_user(user_id)
+            number_processed += 1
+            print datetime.now(), user_id, 'processed.', number_processed, 'of', num_of_users
+            time.sleep(self.time_between_reqs)
+        print 'Scraping finished:', datetime.now()
+
+    def scrape_user(self, user_id):
+        req = requests.get('https://twitter.com/intent/user?user_id=' + user_id)
+
+        # Construct user attributes.
+        attribute_dict = {
+            "id": user_id,
+            "http_status_code": req.status_code
+        }
+
+        # Extract and add useful attributes if status OK.
+        if req.status_code == 200:
+            attribute_dict["screen_name"] = TwitterMetaScraper.extract_screen_name(req)
+            attribute_dict["full_name"] = TwitterMetaScraper.extract_full_name(req)
+
+        # Write extracted attributes to file.
+        with open(self.output_path, 'a') as f:
+            f.write(json.dumps(attribute_dict) + '\n')
+
+    def load_users(self):
+        user_ids = []
+        with open(self.input_path, 'r') as f:
+            for line in f.readlines():
+                user_id = line.split(',')[0].strip()
+                if user_id != '':
+                    user_ids.append(user_id)
+        return user_ids
+
+    @staticmethod
+    def extract_screen_name(req):
+        data = BeautifulSoup(req.content).find('title').string
+        screen_match = re.search('\((@.*)\)', data)
+        if screen_match:
+            return screen_match.group(1).encode('utf-8')
         else:
-            f.write(user_id + ', HTTP-CODE:' + str(r.status_code) + ', ' + 'HTTP-CODE:' + str(r.status_code) + '\n')
-    number_processed += 1
-    print datetime.datetime.now(), user_id, 'processed.', number_processed, 'of', num_of_users
-    time.sleep(time_between_requests)
-print 'Scraping finished:', datetime.datetime.now()
+            return None
+
+    @staticmethod
+    def extract_full_name(req):
+        data = BeautifulSoup(req.content).find('title').string
+        name_match = re.search('(.*)\(@', data)  # Match for full name
+        if name_match:
+            return name_match.group(1).strip().encode('utf-8')
+        else:
+            return None
+
+
+if __name__ == "__main__":
+    scraper = TwitterMetaScraper(input_filename="ids.txt")
+    scraper.run()
